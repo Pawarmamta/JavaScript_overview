5c561110fd2f1d49dba7711c8b4b65ba364b6b35
Mamta pawar <158306301+Pawarmamta@users.noreply.github.com>
2025-08-26 17:33:39 +0000

    DataType & Conversion

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict" //treat all js code as newer version
+//alert(3+3) //using nodejs not browser
+
+//primitive data type
+let name = "mamta"
+let age = 18
+let isLoggedIn = false
+let state ;
+
+/* 
+number = 2^53
+bigint
+string = ""
+boolean = true/false
+null = standlone value (Explicitly empty / nothing value)
+undefine = Absence of value by default
+symbol = unique mostly use in react */ 
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefine
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let value = "mamta"
+
+console.log(typeof value)            //string
+console.log(typeof (value)) //methode // string
+
+let valueInNumber = Number(value);
+console.log(typeof valueInNumber); //number
+console.log(valueInNumber);        // NaN
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+
+let isLoggedIn = "mamta"
+let booleanislogedIn = Boolean(isLoggedIn);
+console.log(typeof booleanislogedIn); 
