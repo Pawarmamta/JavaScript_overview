aeb1aaad60eaca857876bd463cdba62d4efa719a
Mamta pawar <158306301+Pawarmamta@users.noreply.github.com>
2025-08-28 11:47:55 +0000

    Strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,6 @@
+const name = "Mamta";
+const repocount = 23;
+
+//console.log(name + count + "value");
+// string interpolaion:-Inserting variables or expressions directly inside a string.
+console.log(`Hellow my name is ${name} & my total count is ${repocount}`)
