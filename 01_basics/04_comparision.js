112ee5d8687206635be3cce6b04be4bbfa1328a7
Mamta pawar <158306301+Pawarmamta@users.noreply.github.com>
2025-08-28 11:15:21 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,13 @@
+console.log(null ==undefined); //true
+console.log(null ===undefined); //false
+
+console.log(null > 0);  //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+console.log("2" === 2); //false
+
+
+console.log(undefined == 0); //false
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
