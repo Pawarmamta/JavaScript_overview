654a08685503a89dea774026d60de18906ada869
Mamta pawar <158306301+Pawarmamta@users.noreply.github.com>
2025-09-27 16:12:35 +0000

    loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,23 @@
+// for(let i=0; i<=10; i++ )
+// {
+//     console.log(`outer loop ${i}`)
+//     for(let j=0; j<10; j++)
+//     {
+//         console.log (`Inner loop ${j} and inner loop ${i}`)
+//     }
+// }
+
+
+
+// let myArray = [ "flash", "clash", "batman"];
+// console.log (myArray.length);
+// for(let i=0; i<myArray.length; i++)
+// {
+//     const element = myArray[i];
+//     console.log(element);
+
+// }
+
+
+// Break:-break when user reached at condition, 
+// continue:- condition skip for one time 
---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+let i=0;
+while(i<10)
+{
+ console.log(`value is ${i}`);
+ i++
+}
+
+let myArray = ['abc','def','ghi'];
+let arr=0;
+while(arr<myArray.length)
+{
+    console.log(myArray[arr]);
+    arr++;
+}
+
+//////////////////////////////////do while ///////////////////
+
+let c=11;
+do{
+    console.log(`c is ${c}`);
+    c++;
+}while(c<=10);
+//c is 11
