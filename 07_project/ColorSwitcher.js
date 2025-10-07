0408e7f8476d709104fb4337fea92bc28b004d1f
Mamta pawar <158306301+Pawarmamta@users.noreply.github.com>
2025-10-07 17:25:52 +0000

    colorSwitcher

---
+++ 07_project/ColorSwitcher.js
@@ -0,0 +1,28 @@
+const button = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+button.forEach(function(button)
+{
+    button.addEventListener('click', function(e){
+    console.log(e);
+    console.log(e.target);
+    if(e.target.id === 'red')
+    {
+        body.style.backgroundColor = e.target.id;
+    }
+     if(e.target.id === 'blue')
+    {
+        body.style.backgroundColor = e.target.id;
+    }
+     if(e.target.id === 'green')
+    {
+        body.style.backgroundColor = e.target.id;
+    }
+     if(e.target.id === 'orange')
+    {
+        body.style.backgroundColor = e.target.id;
+    }
+
+    
+    })
+});
